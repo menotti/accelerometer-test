fabe383ae3d419ecacf78a6c25d4f2017417c98b
faganellocarlos@gmail.com <faganellocarlos@gmail.com@7a2a9135-9a39-8e07-6eb3-c90716b64e17>
2011-10-12 00:49:33 +0000

    limpando pasta

--- Accelerometer/src/test/accelerometer/AccelerometerListener.java
+++
@@ -1,9 +0,0 @@
-package test.accelerometer;
-
-public interface AccelerometerListener {
-	 
-	public void onAccelerationChanged(float x, float y, float z);
- 
-	public void onShake(float force);
- 
-}
